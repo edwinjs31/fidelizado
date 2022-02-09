74aa951333b9d550775aa50e978e69d6ddcf1422
edwinjs <edwinjs31@gmail.com>
2022-02-09 21:35:04 +0100

    web service de addImage base64 terminado

---
+++ target/fidelizados-1.0/streamedImage.js
@@ -0,0 +1,40 @@
+
+
+var handleFileSelect = function (evt) {
+    var files = evt.target.files;
+    var file = files[0];
+
+    if (files && file) {
+        var reader = new FileReader();
+        
+        reader.onload = function (readerEvt) {
+            var binaryString = readerEvt.target.result;
+            
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/octet-stream");
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: file,
+                redirect: 'follow'
+            };
+
+            fetch("http://localhost:8084/fidelizados/api/addImage/", requestOptions)
+                    .then((response) => response.json())
+                    .then(function (data) {
+                        document.getElementById("responsearea").value = JSON.stringify(data, null, 2);
+                    }).catch(error => console.log('error', error));
+
+        };
+
+        reader.readAsBinaryString(file);
+    }
+};
+
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+    document.getElementById('filePicker').addEventListener('change', handleFileSelect, false);
+} else {
+    alert('The File APIs are not fully supported in this browser.');
+}
+
